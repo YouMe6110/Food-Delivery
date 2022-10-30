dad5daed3d6773709c845b6fa3fde7dd5edfc862
YouMe6110 <ume0116@naver.com>
2022-10-30 16:26:33 +0900

    클래스 및 모델 생성

--- lib/utils/app_constants.dart
+++ lib/utils/app_constants.dart
@@ -5,4 +5,21 @@ class AppConstants {
   static const String baseUrl = "http://mvs.bslmeiyu.com";
   static const String popularProductUri = "/api/v1/products/popular";
   static const String recommendedProductUri = "/api/v1/products/recommended";
+  static const String uploadUri = "$baseUrl/uploads/";
+
+  //Registration
+  static const String registerUri = "/api/v1/auth/register";
+  static const String loginUri = "/api/v1/auth/login";
+  static const String tokenUri = "/authentication/token/new";
+  static const String userInfo = "/api/v1/customer/info";
+
+  //AIzaSyDjHSDTbzDMdMba04MgSMb5dfmwVsh2gII
+
+  static const String token = "token";
+  static const String cartList = "cart-List";
+  static const String cartHistoryList = "cart-History-List";
+  static const String phone = "phone";
+  static const String password = "password";
+
+  static const String apiKey = "";
 }
