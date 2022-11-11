cada851f8f43a8dd11a362951e3b487207ac1f51
YouMe6110 <ume0116@naver.com>
2022-11-11 11:39:49 +0900

    메서드 수정

--- lib/pages/cart/cart_page.dart
+++ lib/pages/cart/cart_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:food_delivery/utils/colors.dart';
 import 'package:food_delivery/utils/dimensions.dart';
 import 'package:food_delivery/widgets/app_icon.dart';
+import 'package:get/get.dart';
 
 class CartPage extends StatelessWidget {
   const CartPage({Key? key}) : super(key: key);
@@ -41,7 +42,46 @@ class CartPage extends StatelessWidget {
                 ),
               ],
             ),
-          )
+          ),
+          Positioned(
+              top: Dimensions.height20 * 5,
+              left: Dimensions.width20,
+              right: Dimensions.width20,
+              bottom: 0,
+              child: Container(
+                  margin: EdgeInsets.only(top: Dimensions.height15),
+                  color: Colors.red,
+                  child: MediaQuery.removePadding(
+                    context: context,
+                    removeTop: true,
+                    child: ListView.builder(
+                        itemCount: 10,
+                        itemBuilder: (_, index) {
+                          return Container(
+                            width: double.maxFinite,
+                            height: Dimensions.height20 * 5,
+                            child: Row(
+                              children: [
+                                Container(
+                                  width: Dimensions.height20 * 5,
+                                  height: Dimensions.height20 * 5,
+                                  margin: EdgeInsets.only(
+                                      bottom: Dimensions.height10),
+                                  decoration: BoxDecoration(
+                                      image: DecorationImage(
+                                          fit: BoxFit.cover,
+                                          image: AssetImage(
+                                              "assets/image/food0.png")),
+                                      borderRadius: BorderRadius.circular(
+                                          Dimensions.radius20),
+                                      color: Colors.white),
+                                ),
+                                Expanded(child: Container())
+                              ],
+                            ),
+                          );
+                        }),
+                  )))
         ],
       ),
     );
