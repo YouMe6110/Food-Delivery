592bf6c366a975858f3908d7a96cb4958d9de1a0
YouMe6110 <ume0116@naver.com>
2022-11-26 22:40:17 +0900

    영구 플러그인 항목 사용

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:food_delivery/pages/home/main_food_page.dart';
 import 'package:food_delivery/utils/colors.dart';
+import 'package:persistent_bottom_nav_bar/persistent-tab-view.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -12,6 +13,7 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   int _selectedIndex = 0;
+  late PersistentTabController _controller;
   List pages = [
     MainFoodPage(),
     Container(child: Center(child: Text("Next page"))),
@@ -19,8 +21,57 @@ class _HomePageState extends State<HomePage> {
     Container(child: Center(child: Text("Next next next page"))),
   ];
 
-  void onTapNav(int index) {}
+  void onTapNav(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
+  @override
+  void initState() {
+    super.initState();
+    _controller = PersistentTabController(initialIndex: 0);
+  }
+
+  List<Widget> _buildScreens() {
+    return [
+      MainFoodPage(),
+      Container(child: Center(child: Text("Next page"))),
+      Container(child: Center(child: Text("Next next page"))),
+      Container(child: Center(child: Text("Next next next page"))),
+    ];
+  }
+
+  List<PersistentBottomNavBarItem> _navBarsItems() {
+    return [
+      PersistentBottomNavBarItem(
+        icon: Icon(CupertinoIcons.home),
+        title: ("Home"),
+        activeColorPrimary: CupertinoColors.activeBlue,
+        inactiveColorPrimary: CupertinoColors.systemGrey,
+      ),
+      PersistentBottomNavBarItem(
+        icon: Icon(CupertinoIcons.settings),
+        title: ("Settings"),
+        activeColorPrimary: CupertinoColors.activeBlue,
+        inactiveColorPrimary: CupertinoColors.systemGrey,
+      ),
+      PersistentBottomNavBarItem(
+        icon: Icon(CupertinoIcons.home),
+        title: ("Home"),
+        activeColorPrimary: CupertinoColors.activeBlue,
+        inactiveColorPrimary: CupertinoColors.systemGrey,
+      ),
+      PersistentBottomNavBarItem(
+        icon: Icon(CupertinoIcons.settings),
+        title: ("Settings"),
+        activeColorPrimary: CupertinoColors.activeBlue,
+        inactiveColorPrimary: CupertinoColors.systemGrey,
+      ),
+    ];
+  }
+
+/*
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -30,15 +81,19 @@ class _HomePageState extends State<HomePage> {
         unselectedItemColor: Colors.amberAccent,
         showSelectedLabels: false,
         showUnselectedLabels: false,
+        selectedFontSize: 0.0,
+        unselectedFontSize: 0.0,
+        currentIndex: _selectedIndex,
         onTap: onTapNav,
         items: const [
           BottomNavigationBarItem(
             icon: Icon(
               Icons.home_outlined,
             ),
-          )
+          ),
         ],
       ),
     );
   }
+  */
 }
