8bdbf4927fc1fc1ab0650af28ed008dcae14bce4
YouMe6110 <ume0116@naver.com>
2022-11-27 21:04:35 +0900

    시작 페이지 만들기

---
+++ lib/pages/splash/splash_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+
+  late Animation<double> animation;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
