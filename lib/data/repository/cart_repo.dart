2d412fb32dbd929363b9b04dbeea0d5fdbdb257f
YouMe6110 <ume0116@naver.com>
2022-11-30 13:32:15 +0900

    cart_repo의 getCartList 메서드 빌드

--- lib/data/repository/cart_repo.dart
+++ lib/data/repository/cart_repo.dart
@@ -1,6 +1,23 @@
+import 'dart:convert';
+import 'package:food_delivery/utils/app_constants.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:food_delivery/models/cart_model.dart';
 
 class CartRepo {
   final SharedPreferences sharedPreferences;
+
   CartRepo({required this.sharedPreferences});
+
+  List<String> cart = [];
+
+  void addToCartList(List<CartModel> cartList) {
+    cart = [];
+    /*convert objects to string because sharedpreferences only accepts string*/
+    cartList.forEach((element) {
+      return cart.add(jsonEncode(element));
+    });
+
+    sharedPreferences.setStringList(AppConstants.CART_LIST, cart);
+    print(sharedPreferences.getStringList(AppConstants.CART_LIST));
+  }
 }
