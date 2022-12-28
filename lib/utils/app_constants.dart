c55bdc9d2ff11f505c2d479206eb1de0bcee3076
YouMe6110 <ume0116@naver.com>
2022-12-28 13:56:28 +0900

    데이터베이스에서 사용자 정보 가져오기

--- lib/utils/app_constants.dart
+++ lib/utils/app_constants.dart
@@ -11,7 +11,7 @@ class AppConstants {
   //auth end points
   static const String REGISTRATION_URI = "/api/v1/auth/register";
   static const String LOGIN_URI = "/api/v1/auth/login";
-  static const String USER_INFO = "/api/v1/customer/info";
+  static const String USER_INFO_URI = "/api/v1/customer/info";
 
   static const String TOKEN = "";
   static const String PHONE = "";
