cc7412dd77319817037e7a8b48cb5ee486e4f4a8
YouMe6110 <ume0116@naver.com>
2023-01-07 13:56:45 +0900

    지연 초기화 오류 해결

--- lib/controllers/location_controller.dart
+++ lib/controllers/location_controller.dart
@@ -3,6 +3,7 @@ import 'package:geocoding/geocoding.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:get/get.dart';
 import 'package:food_delivery/models/address_model.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class LocationController extends GetxController implements GetxService {
   LocationRepo locationRepo;
@@ -23,4 +24,61 @@ class LocationController extends GetxController implements GetxService {
   late Map<String, dynamic> _getAddress;
 
   Map get getAddress => _getAddress;
+
+  late GoogleMapController _mapController;
+  bool _updateAddressData = true;
+  bool _changeAddress = true;
+
+  bool get loading => _loading;
+
+  Position get position => _position;
+
+  Position get pickPosition => _pickPosition;
+
+  void setMapController(GoogleMapController mapController) {
+    _mapController = mapController;
+  }
+
+  void updatePosition(CameraPosition position, bool fromAddress) async {
+    if (_updateAddressData) {
+      _loading = true;
+      update();
+      try {
+        if (fromAddress) {
+          _position = Position(
+              latitude: position.target.latitude,
+              longitude: position.target.longitude,
+              timestamp: DateTime.now(),
+              heading: 1,
+              accuracy: 1,
+              altitude: 1,
+              speedAccuracy: 1,
+              speed: 1);
+        } else {
+          _pickPosition = Position(
+              latitude: position.target.latitude,
+              longitude: position.target.longitude,
+              timestamp: DateTime.now(),
+              heading: 1,
+              accuracy: 1,
+              altitude: 1,
+              speedAccuracy: 1,
+              speed: 1);
+        }
+
+        if (_changeAddress) {
+          String _address = await getAddressfromGeocode(
+              LatLng(position.target.latitude, position.target.longitude));
+        }
+      } catch (e) {
+        print(e);
+      }
+    }
+  }
+
+  Future<String> getAddressfromGeocode(LatLng latLng) async {
+    String _address = "Unknown Location Found";
+    Response response = await locationRepo.getAddressfromGeocode(latLng);
+    return _address;
+  }
 }
